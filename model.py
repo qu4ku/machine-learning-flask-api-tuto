7c77369947bbf9ede808fd901840a2858b62be5f
qu4ku <qu4ku@hotmail.com>
2017-12-19 03:46:19 +0100

    init

---
+++ model.py
@@ -0,0 +1,8 @@
+import os 
+import json
+import numpy as np
+import pandas as pd
+from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.ensemble import RandomForestClassifier
